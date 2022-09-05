d8bda7c8e7dac4bf7400e89be747aaf7863e3381
RRRChangeche <r03522631@ntu.edu.tw>
2022-09-06 00:30:41 +0800

    update yolov4 cfg file

--- DartScore/yolo_console_dll.cpp
+++ DartScore/yolo_console_dll.cpp
@@ -273,7 +273,7 @@ public:
 int main(int argc, char *argv[])
 {
     std::string  names_file = "data/dart.names";
-    std::string  cfg_file = "cfg/yolov4-dart.cfg";
+    std::string  cfg_file = "cfg/yolov4-tiny-dart.cfg";
     std::string  weights_file = "weights/yolov4-tiny-dart_final.weights";
     std::string filename;
 
